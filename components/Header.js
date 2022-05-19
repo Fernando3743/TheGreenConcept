a11c01b129ad28d7bcfdc429bb9b45b1fc4c6cb6
Fercho <luis@dataico.com>
2022-05-18 20:47:59 -0500

    Current progress

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
   return (
     <nav className="fixed inset-x-0 top-0 z-10 bg-transparent text-white bourton-font">
       <div className="max-w-5xl mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          <span className="text-2xl font-semibold">Logo</span>
-          <div className="flex space-x-4 ">
-            <a href="#">Dashboard</a>
-            <a href="#">About</a>
-            <a href="#">Projects</a>
-            <a href="#">Contact</a>
+        <div className="flex items-center justify-between h-16 text-xl">
+            <div>TESTINGGG</div>
+            <div>
+              TEST TEST
+            </div>
+            <div>TESTINGGG</div>
           </div>
         </div>
-      </div>
     </nav>
   );
 };
